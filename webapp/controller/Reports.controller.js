82c17f9229064f3463aa2050ea36e4db87175437
rithika-13 <rithika.a@sap.com>
2024-08-27 19:55:24 +0530

    chart thats is commented

--- webapp/controller/Reports.controller.js
+++ webapp/controller/Reports.controller.js
@@ -108,106 +108,110 @@ sap.ui.define([
 
         onPieChartSelect: function (oEvent) {
 
-            var oData = oEvent.getParameter("data");
-            console.log(oData[0].data.Category)
-            this.byId("chart").setWidth("100%")
-            var oCircletext = this.byId("circletext")
-            oCircletext.setVisible(true)
-            var oCircle = this.byId("circle")
-            oCircle.setVisible(true)
-            var otext = this.byId("valuecircle")
-
-
-            var orecttext = this.byId("recttext")
+            var orecttext = this.byId("sidePanel")
             orecttext.setVisible(true)
-            var orect = this.byId("rect")
-            orect.setVisible(true)
-            var otextrect = this.byId("rectvalue")
-
-            var orecttextleft = this.byId("circletextLeft")
-            orecttextleft.setVisible(true)
-            var orectleft = this.byId("circleLeft")
-            orectleft.setVisible(true)
-            var otextrectleft = this.byId("circlevalueLeft")
-
-
-            var oProductsModel = this.getOwnerComponent().getModel("productsModel");
-            var aProducts = oProductsModel.getProperty("/Products");
-            var oInventoryModel = this.getView().getModel("inventory");
-            var omod = this.getOwnerComponent().getModel("inventory");
-            var transactions = omod.getData()
-            transactions = transactions.InventoryTransactions
-            console.log(transactions)
-            const productPurchaseMap = {};
-
-            // Loop through each transaction
-            transactions.forEach((transaction) => {
-                // Only consider 'IN' (purchased) transactions
-                if (transaction.transaction_type === "OUT" && transaction.category == oData[0].data.Category) {
-                    const productId = transaction.product_name;
-                    const quantity = transaction.quantity;
-                    productPurchaseMap[productId] = []
-                    // Sum up the quantity for each product
-                    if (!productPurchaseMap[productId]) {
-                        productPurchaseMap[productId] = [];
-                    }
-                    if (productPurchaseMap[productId][0]) {
-                        productPurchaseMap[productId][0] += parseInt(quantity);
-                    } else {
-                        productPurchaseMap[productId][0] = parseInt(quantity);
-                    }
-                    productPurchaseMap[productId][1] = parseInt(transaction.price_per_unit);
-                }
-            });
-
-            // const maxValue = Math.max(...Object.values(productPurchaseMap));
-            const maxKey = Object.keys(productPurchaseMap).reduce((a, b) =>
-                productPurchaseMap[a][0] > productPurchaseMap[b][0] ? a : b
-            );
-            let maxProduct = -Infinity; // Initialize with a very low value
-            let prod = ""
-            // Iterate over each key-value pair in the dictionary
-            for (let key in productPurchaseMap) {
-                let values = productPurchaseMap[key];
-
-                // Ensure there are exactly two values in the array
-
-                let product = values[0] * values[1]; // Multiply the two values
-
-                // Update maxProduct if the current product is greater
-                if (product > maxProduct) {
-                    maxProduct = product;
-                    prod = key
-                }
-
-            }
-            console.log(maxProduct, prod)
-
-            const maxValue = productPurchaseMap[maxKey][0];
-
-
-
-
-            var oCategoryStockLevels = {};
-
-            aProducts.forEach(function (product) {
-                var sCategory = product.category; // Assuming each product has a 'category' field
-                var iStockLevel = product.quantity_in_stock; // Assuming each product has a 'quantity_in_stock' field
-
-                // If the category is not yet in the dictionary, initialize it
-                if (!oCategoryStockLevels[sCategory]) {
-                    oCategoryStockLevels[sCategory] = 0;
-                }
-
-                // Add the stock level of the product to its category
-                oCategoryStockLevels[sCategory] += parseInt(iStockLevel);;
-            });
-            otext.setText(oCategoryStockLevels[oData[0].data.Category])
-            otextrect.setText(maxKey + ": " + maxValue);
-            otextrectleft.setText(prod + ":  " + formatter.formatCurrency(maxProduct));
-            // Now, oCategoryStockLevels contains the total stock level for each category
-            console.log(oCategoryStockLevels[oData[0].data.Category]);
-
+            /*
+                        var oData = oEvent.getParameter("data");
+                        console.log(oData[0].data.Category)
+                        this.byId("chart").setWidth("100%")
+                        var oCircletext = this.byId("circletext")
+                        oCircletext.setVisible(true)
+                        var oCircle = this.byId("circle")
+                        oCircle.setVisible(true)
+                        var otext = this.byId("valuecircle")
+            
+            
+                        var orecttext = this.byId("recttext")
+                        orecttext.setVisible(true)
+                        var orect = this.byId("rect")
+                        orect.setVisible(true)
+                        var otextrect = this.byId("rectvalue")
+            
+            
+                        var orecttextleft = this.byId("circletextLeft")
+                        orecttextleft.setVisible(true)
+                        var orectleft = this.byId("circleLeft")
+                        orectleft.setVisible(true)
+                        var otextrectleft = this.byId("circlevalueLeft")
+            
+            
+                        var oProductsModel = this.getOwnerComponent().getModel("productsModel");
+                        var aProducts = oProductsModel.getProperty("/Products");
+                        var oInventoryModel = this.getView().getModel("inventory");
+                        var omod = this.getOwnerComponent().getModel("inventory");
+                        var transactions = omod.getData()
+                        transactions = transactions.InventoryTransactions
+                        console.log(transactions)
+                        const productPurchaseMap = {};
+            
+                        // Loop through each transaction
+                        transactions.forEach((transaction) => {
+                            // Only consider 'IN' (purchased) transactions
+                            if (transaction.transaction_type === "OUT" && transaction.category == oData[0].data.Category) {
+                                const productId = transaction.product_name;
+                                const quantity = transaction.quantity;
+                                productPurchaseMap[productId] = []
+                                // Sum up the quantity for each product
+                                if (!productPurchaseMap[productId]) {
+                                    productPurchaseMap[productId] = [];
+                                }
+                                if (productPurchaseMap[productId][0]) {
+                                    productPurchaseMap[productId][0] += parseInt(quantity);
+                                } else {
+                                    productPurchaseMap[productId][0] = parseInt(quantity);
+                                }
+                                productPurchaseMap[productId][1] = parseInt(transaction.price_per_unit);
+                            }
+                        });
+            
+                        // const maxValue = Math.max(...Object.values(productPurchaseMap));
+                        const maxKey = Object.keys(productPurchaseMap).reduce((a, b) =>
+                            productPurchaseMap[a][0] > productPurchaseMap[b][0] ? a : b
+                        );
+                        let maxProduct = -Infinity; // Initialize with a very low value
+                        let prod = ""
+                        // Iterate over each key-value pair in the dictionary
+                        for (let key in productPurchaseMap) {
+                            let values = productPurchaseMap[key];
+            
+                            // Ensure there are exactly two values in the array
+            
+                            let product = values[0] * values[1]; // Multiply the two values
+            
+                            // Update maxProduct if the current product is greater
+                            if (product > maxProduct) {
+                                maxProduct = product;
+                                prod = key
+                            }
+            
+                        }
+                        console.log(maxProduct, prod)
+            
+                        const maxValue = productPurchaseMap[maxKey][0];
+            
+            
+            
+            
+                        var oCategoryStockLevels = {};
+            
+                        aProducts.forEach(function (product) {
+                            var sCategory = product.category; // Assuming each product has a 'category' field
+                            var iStockLevel = product.quantity_in_stock; // Assuming each product has a 'quantity_in_stock' field
+            
+                            // If the category is not yet in the dictionary, initialize it
+                            if (!oCategoryStockLevels[sCategory]) {
+                                oCategoryStockLevels[sCategory] = 0;
+                            }
+            
+                            // Add the stock level of the product to its category
+                            oCategoryStockLevels[sCategory] += parseInt(iStockLevel);;
+                        });
+                        otext.setText(oCategoryStockLevels[oData[0].data.Category])
+                        otextrect.setText(maxKey + ": " + maxValue);
+                        otextrectleft.setText(prod + ":  " + formatter.formatCurrency(maxProduct));
+                        // Now, oCategoryStockLevels contains the total stock level for each category
+                        console.log(oCategoryStockLevels[oData[0].data.Category]);
+            */
 
         },
         onSideNavButtonPress: function () {
